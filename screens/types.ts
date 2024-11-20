50a356a197a096c0faa6535a78938f6f17309700
lab_services_student <st10442381@vcconnect.edu.za>
2024-11-20 11:52:51 +0200

    modified my types.ts screen

--- screens/types.ts
+++ screens/types.ts
@@ -1,7 +1,6 @@
-// types.ts
+// Updated types.ts
 export type RootStackParamList = {
-    Home: { newItem?: { dishName: string, description: string, course: string, price: number } };
-    AddMenu: undefined;
-    FilterMenu: undefined;
-  };
-  
+  Home: { newItem?: { dishName: string, description: string, course: string, price: number } };
+  AddMenu: undefined;
+  FilterMenu: { menuItems: { dishName: string; description: string; course: string; price: number }[] };
+};
